003db15bf1f6e142ae9a5c98d0600e57ac94d22c
Samuel Kelemen <Samuel@Kelemen.us>
2018-10-19 15:13:53 -0400

    I guess I never committed this

--- CRDT/CRDT/Class1.cs
+++ CRDT/CRDT/Class1.cs
@@ -2,7 +2,63 @@
 
 namespace CRDT
 {
-    public class Class1
+    // monotonic join semi-lattace
+    public class CRDT
     {
+
+    }
+
+    public interface ICRDT<T>
+    {
+        T Merge(params T[] states);
+        T Value();
+    }
+
+    public class GCounter : ICRDT<ulong>
+    {
+        private ulong _localState; 
+        public GCounter(ulong initial = 0)
+        {
+            _localState = initial;
+        }
+        public void Increment()
+        {
+            _localState++;
+        }
+        public ulong Merge(params ulong[] states)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ulong Value()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Integer
+    {
+        public ulong _positive;
+        public ulong _negative;
+
+        public Integer()
+        {
+            _positive = 0L;
+            _negative = 0L;
+        }
+
+        public long Value()
+        {
+            return (long)(_positive - _negative);
+        }
+
+        public void Increment()
+        {
+            _positive++;
+        }
+        public void Decrement()
+        {
+            _negative++;
+        }
     }
 }
